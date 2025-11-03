1fdef31facc9f6d930466d5ffb76d1f1c0c1969f
github-ci <you@example.com>
2025-11-03 14:25:15 +0800

    update++++++++++++

--- src/TrainTeacherBundle.php
+++ src/TrainTeacherBundle.php
@@ -5,6 +5,7 @@ namespace Tourze\TrainTeacherBundle;
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BundleDependency\BundleDependencyInterface;
+use Tourze\EasyAdminMenuBundle\EasyAdminMenuBundle;
 
 class TrainTeacherBundle extends Bundle implements BundleDependencyInterface
 {
@@ -12,6 +13,7 @@ class TrainTeacherBundle extends Bundle implements BundleDependencyInterface
     {
         return [
             DoctrineBundle::class => ['all' => true],
+            EasyAdminMenuBundle::class => ['all' => true],
         ];
     }
 }
